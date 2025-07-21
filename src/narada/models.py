330d34bf3b1ce994c5d3a7ba64f3e44ccbb992c4
Arnav Brahmasandra <arnav@narada.ai>
2025-07-21 13:58:08 -0700

    formatting

--- src/narada/models.py
+++ src/narada/models.py
@@ -4,6 +4,7 @@ from typing import Literal, TypeAlias
 
 UserResourceCredentials: TypeAlias = dict[Literal["salesforce", "jira"], dict[str, str]]
 
+
 class RemoteDispatchChatHistoryItem(BaseModel):
     role: Literal["user", "assistant"]
     content: str
