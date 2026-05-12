3848e38b3539824c51289ccf2adfa5dc4a50bb90
Volodymyr Kasaraba <volodymyr@narada.ai>
2026-05-11 20:38:24 -0400

    upd timeout

--- packages/narada/src/narada/client.py
+++ packages/narada/src/narada/client.py
@@ -279,7 +279,7 @@ class Narada:
         # Navigate to login URL (provided by backend with custom token)
         context = browser.contexts[0]
         initialization_page = context.pages[0]
-        await initialization_page.goto(login_url, timeout=15_000)
+        await initialization_page.goto(login_url, timeout=20_000)
 
         # Wait for browser window ID. The extension can take a bit to be installed, so we retry a
         # few times.
@@ -301,13 +301,13 @@ class Narada:
                 if attempt == max_attempts - 1:
                     raise
                 logging.info("Waiting for Narada extension to authenticate...")
-                await initialization_page.goto(login_url, timeout=15_000)
+                await initialization_page.goto(login_url, timeout=20_000)
             except NaradaTimeoutError:
                 if attempt == max_attempts - 1:
                     raise
                 # If browser window ID is not found, reload the page and try again
                 # try to go to the login URL again (with customToken query param)
-                await initialization_page.goto(login_url, timeout=15_000)
+                await initialization_page.goto(login_url, timeout=20_000)
 
         cloud_window = CloudBrowserWindow(
             browser_window_id=browser_window_id,
